3d6244277947416d1fbab47d15334cedeed7ce98
Mark24 <mark.zhangyoung@qq.com>
2021-11-03 17:53:41 +0800

    feat: add editor

--- src/pages/not-found-page/index.jsx
+++ src/pages/not-found-page/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './not-found.scss'
+import { Link } from "react-router-dom";
 
 export default function NotFoundPage() {
   return (
     <div className="not-found-page">
       Lark Note 404
+      <Link to={'/'}><button>返回首页</button></Link>
     </div>
   )
 }
