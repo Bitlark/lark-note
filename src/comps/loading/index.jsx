de6d102b6fb19d17b471dee376e47ae08f4ca91c
Mark24 <mark.zhangyoung@qq.com>
2021-11-03 18:49:11 +0800

    feat: less code

--- src/comps/loading/index.jsx
+++ src/comps/loading/index.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import './loading.scss';
 
 export default function Loading() {
-  return <img className='circle' src='https://share-img.booster.taptap.com/tap-booster-frontend/common/circle.svg' alt='loading' />
+  return <img className='circle' src={import('./circle.svg')} alt='loading' />
 }
